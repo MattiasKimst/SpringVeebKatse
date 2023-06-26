c7547c6c5775ad8c9349f856f330a06ddf87ec62
MattiasKimst <mattias.kimst@mail.ee>
2023-06-26 22:39:06 +0300

    Initial commit

---
+++ spring5webapp-master/src/main/java/guru/springframework/spring5webapp/SpringVeebKatse.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVeebKatse {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVeebKatse.class, args);
+	}
+}
